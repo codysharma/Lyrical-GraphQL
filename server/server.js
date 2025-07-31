b58d925514f872b6cb660a8ce5334c86538e71b4
codysharma-ga <codysharmageneralassembly@gmail.com>
2025-07-31 11:33:29 -0600

    added env login info

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const models = require('./models');
 const expressGraphQL = require('express-graphql');
@@ -8,7 +9,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your Mongo Atlas URI
-const MONGO_URI = '';
+const MONGO_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rwttmbo.mongodb.net/lyrical?retryWrites=true&w=majority&appName=Cluster0`;
 if (!MONGO_URI) {
   throw new Error('You must provide a Mongo Atlas URI');
 }
